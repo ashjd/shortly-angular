f4f5e59fd57740f3fdaee1bb5195794206bf22e3
ashjd <ashjd29@gmail.com>
2016-05-20 18:57:39 -0700

    Creates link template

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,7 +2,9 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
-  $scope.data = {};
+  $scope.data = {
+    links: []
+  };
   Links.getAll().then(function(links) {
     $scope.data.links = links;
   });
