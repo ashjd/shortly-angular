8915add16a0e0d7c91f56f9143c2b316b2c165f2
ashjd <ashjd29@gmail.com>
2016-05-20 19:22:00 -0700

    Shortened link implement

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -7,17 +7,19 @@ angular.module('shortly.shorten', [])
     url: ''
   };
 
+  $scope.shortLink = [];
+
   $scope.addLink = function() {
-    Links.addOne($scope.link);
-    console.log($scope.link);
+    Links.addOne($scope.link)
+      .then(function(response) {
+        console.log(response.data);
+        $scope.shortLink[0] = response.data;
+      });
   };
 
   $scope.getUrl = function(url) {
     $scope.link.url = url;
     $scope.addLink();
   };
-
-
-
 
 });
